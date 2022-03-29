0c3fdda4fa7c030cacf690ff963fad54da1c0dc3
caseymm <casey@bocoup.com>
2022-03-29 15:56:05 -0700

    change wording to acknowledge texting to unsub

--- src/app-components/success.js
+++ src/app-components/success.js
@@ -6,7 +6,7 @@ class Response extends React.Component {
     return (
       <div className="success">
         <h1>Success!</h1>
-        <p>You are now signed up to receive alerts.</p>
+        <p>You are now signed up to receive alerts. Please reply with "unsubscribe" or "stop" to stop receiving messages.</p>
         <p>Feel free to close this page or
         <Link className="home" to="/">
          view the map of current fires
